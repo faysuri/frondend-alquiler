aed77fe4717a9fa8a0c69e18d20f134f8b8d2b9d
Fay <faysuri03@gmail.com>
2019-11-12 03:39:07 -0500

    agregar registrar

---
+++ src/views/vehiculo.jsx
@@ -0,0 +1,14 @@
+import React, {Component }from 'react'
+import Header from '../components/Headers/Header'
+class Vehiculo extends Component{
+    render(){
+        return(
+            <div> 
+            <Header/>
+                <h1> Vista de vehiculo</h1>
+            </div>
+        );
+    }
+}
+
+export default Vehiculo;
